6c5d5c6e9007ac254ec3767ce5d740515993533a
Hammed Bankole <40712924+bankole874@users.noreply.github.com>
2024-07-19 11:48:21 +0100

    Update variables.tf

---
+++ Assignment/kcvpc-terraform/route_tables/variables.tf
@@ -0,0 +1,24 @@
+variable "name" {
+  description = "The name of the route tables"
+  type        = string
+}
+
+variable "vpc_id" {
+  description = "The ID of the VPC"
+  type        = string
+}
+
+variable "igw_id" {
+  description = "The ID of the Internet Gateway"
+  type        = string
+}
+
+variable "public_subnet_id" {
+  description = "The ID of the public subnet"
+  type        = string
+}
+
+variable "private_subnet_id" {
+  description = "The ID of the private subnet"
+  type        = string
+}
